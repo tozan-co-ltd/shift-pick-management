d4b5e8debe20ebb8996ca5ffcf427b06428ee3b8
優菜 廣田 <y_hirota@tozan.co.jp>
2024-01-19 10:50:30 +0900

    プロジェクト ファイルを追加します。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mar_sumaken_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
