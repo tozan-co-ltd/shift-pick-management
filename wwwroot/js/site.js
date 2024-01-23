a9a73cf09d64c9be8297efa4a137299a25df8cf7
MyMV <hoangmy1992@gmail.com>
2024-01-23 09:15:40 +0900

    ref: トップ画面を追加

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// サイドバーがアクティブ
+$(function () {
+    $("#sidebarToggle").on("click", function () {
+        $("#sidebarToggle").toggleClass("rotateBtn");
+        $("#sidebar").toggleClass("active");
+        $(this).toggleClass("active");
+    });
+});
+
+
+
