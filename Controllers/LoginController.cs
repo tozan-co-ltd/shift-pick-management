1f0eade400127b66722aa76b6cbaffafc3ca3a07
優菜 廣田 <y_hirota@tozan.co.jp>
2024-01-24 11:11:31 +0900

    refactor:using削除

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using mar_sumaken_web.Commons;
+//using mar_sumaken_web.Commons;
 using mar_sumaken_web.Models;
 
 namespace mar_sumaken_web.Controllers
