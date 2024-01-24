39c4ce84c6c7fc8d534db1a7b289dff4090e3119
Khanh_NQ@tozan.co.jp <Khanh_NQ@tozan.co.jp>
2024-01-24 13:49:16 +0900

    feat: ユーザーマスター画面を追加（UIのみ）

---
+++ Controllers/M_UserController.cs
@@ -0,0 +1,76 @@
+﻿using mar_sumaken_web.Commons;
+using mar_sumaken_web.Models;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+using static mar_sumaken_web.Models.M_UserModel;
+
+namespace mar_sumaken_web.Controllers
+{
+    public class M_UserController : Controller
+    {
+        private readonly ILogger<M_UserController> _logger;
+
+        public M_UserController(ILogger<M_UserController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index(M_UserModel model)
+        {
+            string? errorMessage;
+
+            if (model == null)
+                model = new M_UserModel();
+
+            try
+            {
+                // ハンディエラーメッセージ履歴取得
+                //var listShippingImportError = GetListShippingImportError(model);
+
+                //if (listShippingImportError.Count > 0)
+                //{
+                //    IEnumerable<HandyErrorModel> query = listShippingImportError.Select(s => s);
+                //    model.ListShippingImportError = query.ToPagedList();
+                //}
+
+                List<M_User> users = new List<M_User> ();
+
+                for (int i = 4; i < 100; i++)
+                {
+                    M_UserModel.M_User item = new M_UserModel.M_User
+                    { 
+                        UserId = i, LoginId = "sfsd", UserName = "User name " + i,  DepoId = 1, AuthorizedKubun = 1, UpdatedAt = DateTime.Now };
+                        users.Add(item);
+                    }
+
+                if (users.Count > 0)
+                {
+                    IEnumerable<M_User> query = users.Select(s => s);
+                    model.MUserList = query.ToPagedList();
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                // エラーメッセージ取得
+                // 「SQLServerでエラーが発生しました。」
+                errorMessage = ErrorHandling.CreateErrorMessage("E4002");
+
+                // log取得
+                var exceptionMessage = ex.Message;
+                _logger.LogError($"{exceptionMessage} {errorMessage}");
+
+                var shippingImportErrorModel = new HandyErrorMessageModel
+                {
+                    Message = errorMessage + exceptionMessage
+                };
+                return View(shippingImportErrorModel);
+            }
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/M_UserModel.cs
@@ -0,0 +1,108 @@
+﻿using X.PagedList;
+using System.ComponentModel.DataAnnotations;
+
+namespace mar_sumaken_web.Models
+{
+    /// <summary>
+    /// ユーザーマスターのModel
+    /// </summary>
+    public class M_UserModel
+    {
+        public IFormFile PostedFile { get; set; }
+        public string FileName { get; set; }
+        public int UserId { get; set; }
+        public string CopySavePath { get; set; }
+        public IPagedList<M_User> MUserList { get; set; }
+        public class M_User
+        {
+            /// <summary>
+            /// ユーザーid
+            /// </summary>
+            [Display(Name = "ユーザーID")]
+            public int UserId { get; set; }
+
+            /// <summary>
+            /// ログインID
+            /// </summary>
+            [Display(Name = "ログインID")]
+            [Required(ErrorMessage = "E1001 値が未入力です。値を入力してください。")]
+            [RegularExpression(@"[a-zA-Z0-9]+", ErrorMessage = "ログインIDは半角英数字のみ入力できます")]
+            public string LoginId { get; set; }
+
+            /// <summary>
+            /// メイン倉庫ID
+            /// </summary>
+            [Display(Name = "メイン倉庫ID")]
+            public int DepoId { get; set; }
+
+            /// <summary>
+            /// パスワード
+            /// </summary>
+            [Display(Name = "パスワード")]
+            [Required(ErrorMessage = "パスワードは入力必須項目です")]
+            [RegularExpression(@"[a-zA-Z0-9]{4,10}", ErrorMessage = "パスワードは４～10文字入力してください。")]
+            public string Password { get; set; }
+
+            /// <summary>
+            /// ソルト
+            /// </summary>
+            [Display(Name = "ソルト")]
+            public string Salt { get; set; }
+
+            /// <summary>
+            /// 会社名
+            /// </summary>
+            [Display(Name = "会社名")]
+            public string CompanyName { get; set; }
+
+            /// <summary>
+            /// 管理権限区分
+            /// </summary>
+            [Display(Name = "管理権限区分")]
+            public int AuthorizedKubun { get; set; }
+
+            /// <summary>
+            /// ユーザー名
+            /// </summary>
+            [Display(Name = "ユーザー名")]
+            public string UserName { get; set; }
+
+            /// <summary>
+            /// 作成日時
+            /// </summary>
+            [Display(Name = "作成日時")]
+            public DateTime CreatedAt { get; set; }
+
+            /// <summary>
+            /// 作成者
+            /// </summary>
+            [Display(Name = "作成者")]
+            public string CreatedBy { get; set; }
+
+            /// <summary>
+            /// 更新日時
+            /// </summary>
+            [Display(Name = "更新日時")]
+            public DateTime UpdatedAt { get; set; }
+
+            /// <summary>
+            /// 更新者
+            /// </summary>
+            [Display(Name = "更新者")]
+            public string UpdatedBy { get; set; }
+
+            /// <summary>
+            /// 削除フラグ
+            /// </summary>
+            [Display(Name = "削除フラグ")]
+            public int IsDeleted { get; set; }
+
+            /// <summary>
+            /// log用のメッセージ
+            /// </summary>
+            public string Message { set; get; }
+        }
+
+        public List<string> LstErrorMsg { set; get; }//エラーメッセージリスト
+    }
+}
