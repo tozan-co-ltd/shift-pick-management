c51133fb036b88601aeefaca27b1f488cd8c3ce8
Khanh_NQ@tozan.co.jp <Khanh_NQ@tozan.co.jp>
2024-01-24 10:56:53 +0900

    feat: ログイン画面を追加（UIのみ）

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using mar_sumaken_web.Commons;
+using mar_sumaken_web.Models;
+
+namespace mar_sumaken_web.Controllers
+{
+    /// <summary>
+    /// ログイン関係
+    /// </summary>
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// ログイン画面表示
+        /// </summary>
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
