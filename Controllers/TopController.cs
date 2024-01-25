186d3782a0cdc7d3820849ded42fe14fb8357e1f
優菜 廣田 <y_hirota@tozan.co.jp>
2024-01-25 16:00:08 +0900

    refactor:不要コード削除、カラム名修正

--- Controllers/TopController.cs
+++ Controllers/TopController.cs
@@ -30,21 +30,21 @@ namespace mar_sumaken_web.Controllers
             {
                 List<D_HandyErrorMessageModel> handyErrors = new List<D_HandyErrorMessageModel>
                 {
-                    new D_HandyErrorMessageModel { HandyErrorId = 1, ReadingTime = DateTime.Now, HandyMenuName = "Item 1", ErrorMessage = "", CreatedBy = "田中" },
-                    new D_HandyErrorMessageModel { HandyErrorId = 2, ReadingTime = DateTime.Now, HandyMenuName = "Item 2", ErrorMessage = "", CreatedBy = "田中" },
-                    new D_HandyErrorMessageModel { HandyErrorId = 3, ReadingTime = DateTime.Now, HandyMenuName = "Item 3", ErrorMessage = "" , CreatedBy = "田中" },
+                    new D_HandyErrorMessageModel { HandyErrorMessageID = 1, ReadingTime = DateTime.Now, HandyMenuName = "Item 1", ErrorMessage = "", CreatedBy = "田中" },
+                    new D_HandyErrorMessageModel { HandyErrorMessageID = 2, ReadingTime = DateTime.Now, HandyMenuName = "Item 2", ErrorMessage = "", CreatedBy = "田中" },
+                    new D_HandyErrorMessageModel { HandyErrorMessageID = 3, ReadingTime = DateTime.Now, HandyMenuName = "Item 3", ErrorMessage = "" , CreatedBy = "田中" },
                 };
 
                 for (int i = 4; i < 100; i++)
                 {
-                    D_HandyErrorMessageModel item = new D_HandyErrorMessageModel { HandyErrorId = i, ReadingTime = DateTime.Now, HandyMenuName = "Item 1", ErrorMessage = "2432", CreatedBy = "田中" };
+                    D_HandyErrorMessageModel item = new D_HandyErrorMessageModel { HandyErrorMessageID = i, ReadingTime = DateTime.Now, HandyMenuName = "Item 1", ErrorMessage = "2432", CreatedBy = "田中" };
                     handyErrors.Add(item);
                 }
 
                 if (handyErrors.Count > 0)
                 {
                     IEnumerable<D_HandyErrorMessageModel> query = handyErrors.Select(s => s);
-                    model.HandyErrorList = query.ToPagedList();
+                    model.D_HandyErrorMessageList = query.ToPagedList();
                 }
 
                 return View(model);
--- Models/D_HandyErrorMessageModel.cs
+++ Models/D_HandyErrorMessageModel.cs
@@ -5,9 +5,9 @@ namespace mar_sumaken_web.Models
     public class D_HandyErrorMessageModel
     {
         /// <summary>
-        /// 出荷指示取込エラー履歴id
+        /// ハンディエラーメッセージID
         /// </summary>
-        public int HandyErrorId { set; get; }
+        public int HandyErrorMessageID { set; get; }
 
         /// <summary>
         /// 読取日時
@@ -20,29 +20,24 @@ namespace mar_sumaken_web.Models
         public string HandyMenuName { set; get; }
 
         /// <summary>
-        /// エラーメッセージコード
-        /// </summary>
-        public string? ErrorCode { set; get; }
-
-        /// <summary>
         /// エラーメッセージ
         /// </summary>
         public string? ErrorMessage { set; get; }
-
-        /// <summary>
-        /// 登録者
-        /// </summary>
-        public string? CreatedBy { get; set; }
 
         // <summary>
         /// 作成日時
         /// </summary>
         public DateTime CreatedAt { get; set; }
 
-        public IPagedList<D_HandyErrorMessageModel> HandyErrorList {set; get; }//一覧画面のlistを取得と設定
+        /// <summary>
+        /// 登録者
+        /// </summary>
+        public string? CreatedBy { get; set; }
 
-        public string Message { set; get; }
-
+        /// <summary>
+        /// ハンディエラーメッセージリスト
+        /// </summary>
+        public IPagedList<D_HandyErrorMessageModel> D_HandyErrorMessageList {set; get; }
     }
  
 }
