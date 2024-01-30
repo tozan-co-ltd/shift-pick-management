b58e1f7704ce3f29915402a2e6b872d2ebfb7c49
Khanh_NQ@tozan.co.jp <Khanh_NQ@tozan.co.jp>
2024-01-30 16:53:16 +0900

    feat: ユーザーマスター画面を追加（リスト、詳細）

--- ConnectControllers/M_UserConnectController.cs
+++ ConnectControllers/M_UserConnectController.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.Design;
 using System.Data.SqlClient;
 using mar_sumaken_web.Models;
+using static mar_sumaken_web.Models.M_UserModel;
 
 namespace mar_sumaken_web.Commons
 {
@@ -43,6 +44,55 @@ namespace mar_sumaken_web.Commons
         }
 
         /// <summary>
+        /// ユーザーマスターリストの詳細を取得する
+        /// </summary>
+        /// <param name="userList"></param>
+        /// <param name="databaseName"></param>
+        /// <returns></returns>
+        public static List<M_UserModel.M_User> GetUserListDetail(List<M_User> userList, string databaseName)
+        {
+            try
+            {
+                // SQLServer接続文字列取得
+                var connectionString = ConnectToSQLServer.GetSQLServerConnectionString(databaseName);
+                // SQLServer接続
+                using (var connection = new SqlConnection())
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+
+                    // m_depo
+                    if (userList.Count > 0)
+                    {
+                        foreach (M_User user in userList)
+                        {
+                            // 倉庫マスター情報取得
+                            var userDepoSql = CreateSQLToGetUserDepoList(user.UserID);
+                            List<M_DepoModel> depoList = connection.Query<M_DepoModel>(userDepoSql).ToList();
+                            if(depoList.Count > 0)
+                            {
+                                user.M_DepoList = depoList;
+                            }
+
+                            // ハンディメニューマスター情報取得
+                            var userHandyMenuSql = CreateSQLToGetUserHandyMenuList(user.UserID);
+                            List<M_HandyMenuModel> handyMenuList = connection.Query<M_HandyMenuModel>(userHandyMenuSql).ToList();
+                            if (handyMenuList.Count > 0)
+                            {
+                                user.M_HandyMenuList = handyMenuList;
+                            }
+                        }
+                    }
+                }
+                return userList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
         /// ユーザーマスターSELECT文SQL作成
         /// </summary>
         /// <returns>SQL</returns>
@@ -55,6 +105,12 @@ namespace mar_sumaken_web.Commons
                         ,UserName
                         ,DepoID
                         ,AuthorizedKubun
+                        ,CASE 
+                            WHEN AuthorizedKubun = 1 THEN '管理者'
+                            WHEN AuthorizedKubun = 2 THEN '作業者'
+                            WHEN AuthorizedKubun = 3 THEN '作業者(解除要)'
+                            ELSE''
+                         END AS AuthorizedKubunName
                         ,FORMAT (CreatedAt, 'yyyy/MM/dd ')     AS CreatedAt
                         ,CreatedBy
                         ,FORMAT (UpdatedAt, 'yyyy/MM/dd ')     AS UpdatedAt
@@ -82,6 +138,66 @@ namespace mar_sumaken_web.Commons
 
             return sql;
         }
-       
+
+        /// <summary>
+        /// ユーザーマスター情報取得SQL作成
+        /// </summary>
+        /// <returns>SQL</returns>
+        public static string CreateSQLToGetMUsers()
+        {
+            var sql = CreateSQLToSelectMUsers();
+            sql += $@"
+                    WHERE
+                        NotUseFlag = 0
+                ";
+
+            return sql;
+        }
+
+        /// <summary>
+        /// 倉庫マスター情報取得SQL作成
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public static string CreateSQLToGetUserDepoList(int userId)
+        {
+            var sql = $@"
+                        SELECT 
+	                        userDepo.DepoID,
+	                        m_depo.DepoCode,
+	                        m_depo.DepoName
+                        FROM 
+	                        R_UserDepo AS userDepo
+                        INNER JOIN M_Depo AS m_depo 
+                            ON userDepo.DepoID = m_depo.DepoID
+                        WHERE 
+	                        userDepo.UserID = {userId}
+                            AND m_depo.NotUseFlag = 0
+                    ";
+            return sql;
+        }
+
+        /// <summary>
+        /// ハンディメニューマスター情報取得SQL作成
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public static string CreateSQLToGetUserHandyMenuList(int userId)
+        {
+            var sql = $@"
+                         SELECT 
+	                        userMenu.HandyMenuID,
+	                        menu.HandyMenuName
+                         FROM 
+	                        R_UserHandyMenu AS userMenu
+                         INNER JOIN M_HandyMenu AS menu 
+                            ON userMenu.HandyMenuID = menu.HandyMenuID
+                         WHERE 
+	                        userMenu.UserID = {userId}
+                            AND menu.NotUseFlag = 0
+                    ";
+            return sql;
+        }
+
     }
 }
---
+++ Controllers/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mar_sumaken_web.Models;
+
+namespace mar_sumaken_web.Controllers
+{
+
+    public class BaseController : Controller
+    {
+        // ログイン中ユーザー情報取得
+        public LoginUserModel UserDataList()
+        {
+            var userData = User.Claims.ToList();
+            try
+            {
+                var userModel = new LoginUserModel
+                {
+                    CompanyID = Convert.ToInt32(userData.Where(x => x.Type == "CompanyID").First().Value),
+                    CompanyCode = userData.Where(x => x.Type == "CompanyCode").First().Value,
+                    CompanyName = userData.Where(x => x.Type == "CompanyName").First().Value,
+                    DatabaseName = userData.Where(x => x.Type == "DatabaseName").First().Value,
+                    UserID = Convert.ToInt32(userData.Where(x => x.Type == "UserID").First().Value),
+                    UserName = userData.Where(x => x.Type == "UserName").First().Value,
+                    Role = Convert.ToInt32(userData.Where(x => x.Type == "Role").First().Value),
+                    MainDepoID = Convert.ToInt32(userData.Where(x => x.Type == "MainDepoID").First().Value),
+                    MainDepoName = userData.Where(x => x.Type == "MainDepoName").First().Value,
+                    AuthorizedKubun = Convert.ToInt32(userData.Where(x => x.Type == "AuthorizedKubun").First().Value),
+                    TimeStamp = Convert.ToDateTime(userData.Where(x => x.Type == "TimeStamp").First().Value)
+                };
+                return userModel;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+
+}
