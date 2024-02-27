c9ea07f2dcf0e122c209a727e6a8de6b86d02ecf
Khanh_NQ@tozan.co.jp <Khanh_NQ@tozan.co.jp>
2024-02-27 16:03:26 +0900

    feat: 入荷実績検索を追加

---
+++ ConnectControllers/D_ReceiveConnectController.cs
@@ -0,0 +1,99 @@
+﻿using Dapper;
+using mar_sumaken_web.Models;
+using System.Data.SqlClient;
+
+namespace mar_sumaken_web.Commons
+{
+    /// <summary>
+    /// 入荷実績テーブルに関する関数
+    /// </summary>
+    public static class D_ReceiveConnectController
+    {
+        /// <summary>
+        /// 入荷実績情報取得
+        /// </summary>
+        /// <param name="sql">SQL文</param>
+        /// <param name="databaseName">データベース名</param>
+        /// <returns></returns>
+        public static List<D_ReceiveModel> ConnectDReceives(string sql, string databaseName)
+        {
+            // 戻り値
+            List<D_ReceiveModel> strList = new();
+
+            // DB接続
+            try
+            {
+                // SQLServer接続文字列取得
+                var connectionString = ConnectToSQLServer.GetSQLServerConnectionString(databaseName);
+                // SQLServer接続
+                using (var connection = new SqlConnection())
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+
+                    strList = connection.Query<D_ReceiveModel>(sql).ToList();
+                }
+                return strList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 入荷実績情報取得SQL作成
+        /// </summary>
+        /// <param name="start">入庫日開始</param>
+        /// <param name="end">入庫日終了</param>
+        /// <param name="depoId">倉庫ID</param>
+        /// <param name="supplierId">会社ID</param>
+        /// <returns>SQL文</returns>
+        public static string CreateSQLToGetDReceives(string start, string end, int depoId, int supplierId)
+        {
+            end = string.Concat(end, " 23:59:59");
+            var sql = $@"
+                SELECT
+                    dReceive.ReceiveID
+                    ,dReceive.ScanResultID
+                    ,dReceive.CompanyID AS SupplierID
+                    ,company.CompanyName AS SupplierName
+                    ,dReceive.ReceiveDatetime
+                    ,dReceive.SupplierProductNumber
+                    ,dReceive.LotNumber
+                    ,dReceive.MainProductKey
+                    ,dReceive.FirstSubProductKey
+                    ,dReceive.SecondSubProductKey
+                    ,dReceive.NumberOfBoxes
+                    ,dReceive.Quantity
+                    ,dReceive.ScanedAt
+                    ,dReceive.CreatedAt
+                    ,dReceive.CreatedBy
+                    ,depo.DepoName AS DepoName
+                    ,scan.HandyMenuID
+                    ,scan.SupplierKanbanID
+                    ,scan.NumberOfInputBoxes
+                    ,scan.FirstScanedString
+                    ,scan.SecondScanedString
+                    ,scan.CreatedBy AS ScanCreatedBy
+                FROM D_Receive dReceive
+                INNER JOIN D_ScanResult AS scan 
+	                ON dReceive.ScanResultID = scan.ScanResultID
+                INNER JOIN M_Company AS company 
+                        ON dReceive.CompanyID = company.CompanyID
+                INNER JOIN M_Depo AS depo 
+                    ON scan.DepoID = depo.DepoID
+                WHERE 
+                    scan.DepoID = {depoId}
+                    AND dReceive.CompanyID = {supplierId}
+                    AND dReceive.ReceiveDatetime >= '{start}'
+                    AND dReceive.ReceiveDatetime <= '{end}'
+	                AND company.IsDeleted = 0
+                    AND depo.IsDeleted = 0
+                ORDER BY 
+	                dReceive.SupplierProductNumber ASC 
+            ";
+            return sql;
+        }
+    }
+}
--- Controllers/D_ReceiveController.cs
+++ Controllers/D_ReceiveController.cs
@@ -2,6 +2,8 @@
 using mar_sumaken_web.Models;
 using mar_sumaken_web.Properties;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using X.PagedList;
 
 namespace mar_sumaken_web.Controllers
 {
@@ -33,6 +35,92 @@ namespace mar_sumaken_web.Controllers
             {
                 ViewData["ErrorMessage"] = ErrorMessagesResources.E9999;
                 return View(model);
+            }
+        }
+
+        /// <summary>
+        /// 入荷実績情報取得
+        /// </summary>
+        /// <param name="searchModel">検索モデル</param>
+        /// <returns></returns>
+        public IActionResult SearchData(D_ReceiveModel searchModel)
+        {
+            D_ReceiveModel model = new();
+            var searchData = string.Empty;
+            try
+            {
+                // ログイン中ユーザー情報取得
+                var user = ClaimsLoginUserData();
+
+                // 検索情報をチェック
+                if (!ModelState.IsValid)
+                {
+                    return NotFound(new { errorMessage = "正しい入力値を入力してください。" });
+                }
+
+                // 入庫実績情報取得SQL作成
+                var sql = D_ReceiveConnectController.CreateSQLToGetDReceives(
+                    searchModel.SearchStartDate, searchModel.SearchEndDate, searchModel.SelectedDepoID, searchModel.SelectedCompanyID);
+                // DB接続
+                List<D_ReceiveModel> searchList = D_ReceiveConnectController.ConnectDReceives(sql, user.DatabaseName);
+
+                // 表示用のhtml作成
+                if (searchList.Count > 0)
+                {
+                    model.D_ReceiveModelList = searchList.ToPagedList();
+
+                    foreach (var item in model.D_ReceiveModelList)
+                    {
+                        searchData += $@"<tr>
+                        <td>
+                            <a class='btn btn-success btn-icon-split ml-1 mr-1'
+                            onclick='OnEditClick(this)' data-id='{item.ReceiveID}' data-toggle='modal' data-target='#edit-modal'>
+                                <i class='fa-solid fa-pen'></i>
+                            </a>
+                            <button class='btn btn-danger btn-icon-split'
+                            onclick='OnDeleteClick(this)' data-id='{item.ReceiveID}' data-toggle='modal' data-target='#delete-modal'>
+                                <i class='fa-solid fa-trash'></i>
+                            </button>
+                        </td>
+                        <td class='ReceiveID'>{@item.ReceiveID}</td>
+                        <td class='SupplierName'>{@item.SupplierName}</td>
+                        <td class='ReceiveDatetime'>{@item.ReceiveDatetime}</td>
+                        <td class='SupplierProductNumber'>{@item.SupplierProductNumber}</td>
+                        <td class='LotNumber'>{@item.LotNumber}</td>
+                        <td class='NumberOfBoxes'>{@item.NumberOfBoxes}</td>
+                        <td class='Quantity'>{@item.Quantity}</td>
+                        <td class='MainProductKey'>{@item.MainProductKey}</td>
+                        <td class='FirstSubProductKey'>{@item.FirstSubProductKey}</td>
+                        <td class='SecondSubProductKey'>{@item.SecondSubProductKey}</td>
+                        <td class='CreatedAt'>{@item.CreatedAt.ToString("yyyy/MM/dd")}</td>
+                        <td class='ScanedAt'>{@item.ScanedAt.ToString("yyyy/MM/dd")}</td>
+                        <td class='CreatedBy'>{@item.CreatedBy}</td>
+                        <input type='hidden' class='DepoID' value='{item.DepoID}' />
+                        <input type='hidden' class='SupplierID' value='{item.SupplierID}' />
+                        <input type='hidden' class='ScanResultID' value='{item.ScanResultID}'/>
+    
+                        <input type='hidden' class='HandyMenuID' value='{item.HandyMenuID}'/>
+                        <input type='hidden' class='SupplierKanbanID' value='{item.SupplierKanbanID}'/>
+                        <input type='hidden' class='NumberOfInputBoxes' value='{item.NumberOfInputBoxes}'/>
+                        <input type='hidden' class='FirstScanedString' value='{item.FirstScanedString}'/>
+                        <input type='hidden' class='SecondScanedString' value='{item.SecondScanedString}'/>
+                        <input type='hidden' class='NumberOfInputBoxes' value='{item.NumberOfInputBoxes}'/>
+                        <input type='hidden' class='FirstScanedString' value='{item.FirstScanedString}'/>
+                        <input type='hidden' class='SecondScanedString' value='{item.SecondScanedString}'/>
+                        </tr>";
+                    }
+                }
+
+                return Content(searchData);
+            }
+            catch (SqlException)
+            {
+                return NotFound(new { errorMessage = "E3004: " + ErrorMessagesResources.E3004 });
+            }
+            catch (Exception ex)
+            {
+                var exceptionMessage = ex.Message;
+                return Content(exceptionMessage);
             }
         }
     }
