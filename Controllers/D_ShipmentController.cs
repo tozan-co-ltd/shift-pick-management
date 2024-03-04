b8d0366a16c802627dc503f35c400e7ab796547a
優菜 廣田 <y_hirota@tozan.co.jp>
2024-03-04 17:41:38 +0900

    style:アクセス権限チェック不要のため削除,ヘッダー名修正

--- Controllers/D_ShipmentController.cs
+++ Controllers/D_ShipmentController.cs
@@ -25,13 +25,6 @@ namespace mar_sumaken_web.Controllers
                 // ログイン中ユーザー情報取得
                 var user = ClaimsLoginUserData();
 
-                // 管理権限区分が1(管理者)でない場合はエラーとする
-                if (user == null || user.AuthorizedKubun != 1)
-                {
-                    ViewData["ErrorMessage"] = "E1015: " + ErrorMessagesResources.E1015;
-                    return View(model);
-                }
-
                 // 出荷実績情報取得SQL作成
                 string sql = D_ShipmentConnectController.CreateSQLToSelectDShipments(
                     shipmentScheduleId, depoId, companyId);
