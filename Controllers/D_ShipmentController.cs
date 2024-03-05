f41c3864179a47525a28df6d3d94abd48753df21
優菜 廣田 <y_hirota@tozan.co.jp>
2024-03-05 11:25:12 +0900

    refactor:出荷実績表示のエラー分岐削除

--- Controllers/D_ShipmentController.cs
+++ Controllers/D_ShipmentController.cs
@@ -26,46 +26,29 @@ namespace mar_sumaken_web.Controllers
                 // ログイン中ユーザー情報取得
                 var user = ClaimsLoginUserData();
 
-                // 倉庫IDで倉庫情報を取得
+                // 倉庫IDから倉庫情報を取得
                 var searchDepoSql = M_DepoConnectController.CreateSQLToSelectByDepoId(depoId);
                 List <M_DepoModel> depoSearchList = M_DepoConnectController.ConnectMDepos(searchDepoSql, user.DatabaseName);
-                if (depoSearchList.Count != 1)
-                {
-                    ViewData["ErrorMessage"] = "E1015: " + ErrorMessagesResources.E1015;
-                    return View(model);
-                }
-                // 会社IDで会社情報を取得
+
+                // 会社IDから会社情報を取得
                 var searchCompanySql = M_CompanyConnectController.CreateSQLToSelectByCompanyId(companyId);
                 List<M_CompanyModel> companySearchList = M_CompanyConnectController.ConnectMCompanys(searchCompanySql, user.DatabaseName);
-                if (companySearchList.Count != 1)
-                {
-                    ViewData["ErrorMessage"] = "E1015: " + ErrorMessagesResources.E1015;
-                    return View(model);
-                }
-
-                // 出荷実績情報取得SQL作成
-                string sql = D_ShipmentConnectController.CreateSQLToSelectDShipments(
-                    shipmentScheduleId, depoId, companyId);
 
                 // 出荷実績情報取得
-                List<D_ShipmentModel> searchList = D_ShipmentConnectController.ConnectDShipments(sql, user.DatabaseName);
-                if (searchList.Count <= 0)
-                {
-                    ViewData["ErrorMessage"] = ErrorMessagesResources.E9999;
-                    return View(model);
-                }
+                string sql = D_ShipmentConnectController.CreateSQLToSelectDShipments(
+                    shipmentScheduleId, depoId, companyId);
+                List<D_ShipmentModel> dShipmentList = D_ShipmentConnectController.ConnectDShipments(sql, user.DatabaseName);
                 
-                model.DShipmentList = searchList;
+                model.DShipmentList = dShipmentList;
                 model.DepoName = depoSearchList[0].DepoName;
-                model.DepoID = depoId;
                 model.DeliveryName = string.Concat(companySearchList[0].CompanyName, " - ", companySearchList[0].ClientName);
-                model.DeliveryID = companyId;
 
                 return View(model);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                ViewData["ErrorMessage"] = ErrorMessagesResources.E9999;
+                var errorMessage = "E9999: " + ErrorMessagesResources.E9999;
+                ViewData["ErrorMessage"] = errorMessage + ex.Message;
                 return View(model);
             }
         }
