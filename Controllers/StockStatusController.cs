27e93fdf67e64c0a960a4b7e3eb8932f9a8bac56
Khanh_NQ@tozan.co.jp <Khanh_NQ@tozan.co.jp>
2024-03-14 14:30:26 +0900

    feat: ロット番号がblankのデータはモーダルに表示しない (品番別ロット番号一覧)

--- Controllers/StockStatusController.cs
+++ Controllers/StockStatusController.cs
@@ -214,7 +214,7 @@ namespace mar_sumaken_web.Controllers
 
                 // 日別在庫照会画面表示
                 List<StockStatusModel> searchProductResult = GetLotNumberDetail(searchDate, depoId, companyId, supplierProductNumber, user.DatabaseName);
-                detailList = searchProductResult.Where(item => item.StockRemainQuantity != 0).ToList();
+                detailList = searchProductResult.Where(item => item.StockRemainQuantity > 0 && !string.IsNullOrEmpty(item.LotNumber)).ToList();
 
                 var searchData = string.Empty;
                 // 表示用のhtml作成
