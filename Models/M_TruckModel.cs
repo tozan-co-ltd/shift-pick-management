5884cbbaaa30a8418505a6dba84535fa13e5e18d
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-09-24 17:18:43 +0900

    [feat]:車両モデルの作成

---
+++ Models/M_TruckModel.cs
@@ -0,0 +1,48 @@
+﻿namespace ai_truck_load_measurement.Models
+{
+    /// <summary>
+    /// 車両マスターのモデル
+    /// </summary>
+    public class M_TruckModel: CommonModel
+    {
+        /// <summary>
+        /// 車両ID
+        /// </summary>
+        public int TruckID {  get; set; }
+
+        /// <summary>
+        /// 車両番号
+        /// </summary>
+        public int TruckNumber {  get; set; }
+
+        /// <summary>
+        /// 識別番号
+        /// </summary>
+        public int IdentifyNumber {  get; set; }
+
+        /// <summary>
+        /// 削除フラグ
+        /// </summary>
+        public bool IsDeleted {  get; set; }
+
+        /// <summary>
+        /// 作成日時
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// 作成者
+        /// </summary>
+        public string CreatedBy {  get; set; }
+
+        /// <summary>
+        /// 更新日時
+        /// </summary>
+        public DateTime UpdatedAt { get; set; }
+
+        /// <summary>
+        /// 更新者
+        /// </summary>
+        public string UpdatedBy { get; set; }
+    }
+}
