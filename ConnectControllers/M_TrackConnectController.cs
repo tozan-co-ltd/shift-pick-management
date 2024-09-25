c362035089c4c1421b2e3da82a43b28dde56edc8
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-09-25 16:09:19 +0900

    [feat]:車両マスターのSql操作に関するクラスM_TruckConnectControllerを作成

---
+++ ConnectControllers/M_TrackConnectController.cs
@@ -0,0 +1,304 @@
+﻿using Dapper;
+using ai_truck_load_measurement.Commons;
+using ai_truck_load_measurement.Models;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// 車両マスターに関する関数
+/// </summary>
+namespace ai_truck_load_measurement.ConnectControllers
+{
+    public class M_TrackConnectController
+    {
+        /// <summary>
+        /// 車両情報取得
+        /// </summary>
+        /// <param name="sql">SQL文</param>
+        /// <param name="databaseName">データベース名</param>
+        /// <returns></returns>
+        public static List<M_TruckModel> ConnectMTrucks(string sql, string databaseName)
+        {
+            // 戻り値
+            List<M_TruckModel> strList = new();
+
+            // DB接続
+            try
+            {
+                // SQLServer接続文字列取得
+                var connectionString = ConnectToSQLServer.GetSQLServerConnectionString(databaseName);
+                // SQLServer接続
+                using (var connection = new SqlConnection())
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+
+                    strList = connection.Query<M_TruckModel>(sql).ToList();
+                }
+                return strList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 車両情報登録
+        /// </summary>
+        /// <param name="model">登録情報</param>
+        /// <param name="loginUser">ログインユーザー情報</param>
+        /// <returns>インサート数</returns>
+        public static int InsertMTruck(M_TruckModel model, LoginUserModel loginUser)
+        {
+            // SQLServer接続文字列取得
+            var connectionString = ConnectToSQLServer.GetSQLServerConnectionString(loginUser.DatabaseName);
+            // SQLServer接続
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                // DB接続
+                try
+                {
+                    DateTime sysDate = DateTime.Now;
+                    string sql = CreateSQLToInsertMTruck(model, sysDate, loginUser.UserName);
+                    var insertedCount = connection.Execute(sql);
+
+                    return insertedCount;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 車両情報更新
+        /// </summary>
+        /// <param name="model">登録情報</param>
+        /// <param name="loginUser">ログインユーザー情報</param>
+        /// <returns>更新件数</returns>
+        public static int UpdateMTruck(M_TruckModel model, LoginUserModel loginUser)
+        {
+            // SQLServer接続文字列取得
+            var connectionString = ConnectToSQLServer.GetSQLServerConnectionString(loginUser.DatabaseName);
+            // SQLServer接続
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                // DB接続
+                try
+                {
+                    DateTime sysDate = DateTime.Now;
+                    string sql = CreateSQLToUpdateMTruck(model, sysDate, loginUser.UserName);
+                    var count = connection.Execute(sql);
+
+                    return count;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 車両情報削除
+        /// </summary>
+        /// <param name="truckId">車両ID</param>
+        /// <param name="loginUser">ログインユーザー情報</param>
+        /// <returns>更新件数</returns>
+        public static int DeleteMTruck(int truckId, LoginUserModel loginUser)
+        {
+            // SQLServer接続文字列取得
+            var connectionString = ConnectToSQLServer.GetSQLServerConnectionString(loginUser.DatabaseName);
+            // SQLServer接続
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                // DB接続
+                try
+                {
+                    DateTime sysDate = DateTime.Now;
+                    string sql = CreateSQLToDeleteMTruck(truckId, sysDate, loginUser.UserName);
+                    var count = connection.Execute(sql);
+
+                    return count;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 車両マスター情報取得SQL作成
+        /// </summary>
+        /// <returns>SQL文</returns>
+        public static string CreateSQLToSelectMTrucks()
+        {
+            var sql = $@"
+                SELECT 
+	                truck_id
+                    ,truck_code
+                    ,truck_name
+                    ,is_deleted
+                    ,created_at
+                    ,created_by
+                    ,updated_at
+                    ,updated_by
+                FROM 
+	                m_truck
+                WHERE 
+                    is_deleted = 0
+            ";
+            return sql;
+        }
+
+        /// <summary>
+        /// 重複車両情報取得SQL作成
+        /// </summary>
+        /// <param name="truckNumber">車両コード</param>
+        /// <returns>SQL文</returns>
+        public static string CreateSQLToSelectDuplicateMTruck(int truckNumber)
+        {
+            var sql = $@"
+                SELECT
+                    COUNT(*)                      
+                FROM 
+                    m_truck
+                WHERE
+                    truck_number = {truckNumber}
+                    AND is_deleted = 0
+            ";
+
+            return sql;
+        }
+
+        /// <summary>
+        /// 異なるIDで重複車両情報取得SQL作成
+        /// </summary>
+        /// <param name="model">登録情報</param>
+        /// <returns>SQL文</returns>
+        public static string CreateSQLToSelectDuplicateEditMTruck(M_TruckModel model)
+        {
+            var sql = $@"
+                SELECT
+                    COUNT(*)                      
+                FROM 
+                    m_truck
+                WHERE
+                    truck_number = {model.TruckNumber}
+                    AND truck_id <> {model.TruckID}
+                    AND is_deleted = 0
+            ";
+
+            return sql;
+        }
+
+        /// <summary>
+        /// 車両IDで車両情報を取得
+        /// </summary>
+        /// <param name="truckId">車両ID</param>
+        /// <returns></returns>
+        public static string CreateSQLToSelectByTruckId(int truckId)
+        {
+            var sql = $@"
+                    SELECT
+                        *               
+                    FROM 
+                        m_truck
+                    WHERE
+                        truck_id = {truckId}
+                        AND is_deleted = 0
+            ";
+
+            return sql;
+        }
+
+        /// <summary>
+        /// 車両マスター登録SQL作成
+        /// </summary>
+        /// <param name="model">登録情報</param>
+        /// <param name="createdAt">システムタイム</param>
+        /// <param name="createdBy">ユーザー名</param>
+        /// <returns>SQL文</returns>
+        private static string CreateSQLToInsertMTruck(M_TruckModel model, DateTime createdAt, string createdBy)
+        {
+            string formatCreatedAt = createdAt.ToString("yyyy/MM/dd HH:mm:ss");
+
+            var sql = $@"
+                INSERT INTO m_trucks(
+                    truck_number, 
+                    identify_number,
+                    created_at,
+                    created_by,
+                    update_at,
+                    update_by
+                )
+                VALUES (
+                    '{model.TruckNumber}',
+                    '{model.IdentifyNumber}',
+                    '{formatCreatedAt}',
+                    '{createdBy}',
+                    '{formatCreatedAt}',
+                    '{createdBy}'
+                );
+            ";
+            return sql;
+        }
+
+        /// <summary>
+        /// 車両マスター更新SQL作成
+        /// </summary>
+        /// <param name="model">更新情報</param>
+        /// <param name="updatedAt">システムタイム</param>
+        /// <param name="updatedBy">ユーザー名</param>
+        /// <returns>SQL文</returns>
+        private static string CreateSQLToUpdateMTruck(M_TruckModel model, DateTime updatedAt, string updatedBy)
+        {
+            var sql = $@"
+                UPDATE m_trucks
+                SET 
+                    truck_number = '{model.TruckNumber}',
+                    identify_number = '{model.IdentifyNumber}',
+                    updated_at = '{updatedAt}',
+                    updated_by = '{updatedBy}'
+                WHERE
+                    truck_id = {model.TruckID}
+                    and IsDeleted = 0
+            ";
+            return sql;
+        }
+
+        /// <summary>
+        /// 車両マスター削除SQL作成
+        /// </summary>
+        /// <param name="truckId"></param>
+        /// <param name="updatedAt"></param>
+        /// <param name="updatedBy"></param>
+        /// <returns>SQL文</returns>
+        private static string CreateSQLToDeleteMTruck(int truckId, DateTime updatedAt, string updatedBy)
+        {
+            var sql = $@"
+                UPDATE m_trucks
+                SET 
+                    is_deleted = 1,
+                    updated_at = '{updatedAt}',
+                    updated_by = '{updatedBy}'
+                WHERE 
+                    truck_id = {truckId}
+            ;";
+            return sql;
+        }
+    }
+}
+
