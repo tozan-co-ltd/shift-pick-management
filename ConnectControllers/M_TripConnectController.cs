6ccb9ce4f21ff75ed38f467e052e371677cef7a4
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-10-01 16:24:59 +0900

    [feat]:便マスター登録画面仮組

---
+++ ConnectControllers/M_TripConnectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ai_truck_load_measurement.ConnectControllers
+{
+    public class M_TripConnectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/M_TripController.cs
+++ Controllers/M_TripController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ai_truck_load_measurement.Models;
+using ai_truck_load_measurement.Properties;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ai_truck_load_measurement.Controllers
 {
@@ -6,7 +8,27 @@ namespace ai_truck_load_measurement.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            M_TripModel model = new();
+            return View(model);
+        }
+
+        /// <summary>
+        /// 便マスター登録画面表示
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Register()
+        {
+            M_TripModel model = new();
+            try
+            {
+                return View(model);
+            }
+            catch (Exception)
+            {
+                ViewData["ErrorMessage"] = "E9999: " + ErrorMessagesResources.E9999;
+                return View(model);
+            }
         }
     }
 }
