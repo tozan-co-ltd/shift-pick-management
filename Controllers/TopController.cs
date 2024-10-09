58b46fa2600193781d034aaa2ce971ce55c43231
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-10-09 14:56:01 +0900

    [feat]:トップ画面のmvcそれぞれから不要なコードを削除

--- Controllers/TopController.cs
+++ Controllers/TopController.cs
@@ -31,24 +31,6 @@ namespace ai_truck_load_measurement.Controllers
                 // ログイン中ユーザー情報取得
                 var user = ClaimsLoginUserData();
 
-                // ハンディエラーメッセージ取得
-                var sql = D_HandyErrorMessageConnectController.CreateSQLToSelectDHandyErrorMessages();
-                IEnumerable<D_HandyErrorMessageModel> handyErrorMessageList = D_HandyErrorMessageConnectController.ConnectDHandyErrorMessages(sql, user.DatabaseName);
-                topModel.D_HandyErrorMessageList = handyErrorMessageList.ToPagedList();
-
-                // 表示する出荷作業進捗は、会社コード=10001(豊田自動織機)固定
-                var mCompany = M_CompanyConnectController.GetMCompanyByCompanyCode("10001", user.DatabaseName);
-
-                // 納入指示日は翌日(土日を除く)
-                var nextDay = Utils.GetNextday(DateTime.Now);
-                topModel.NextDayGraph = GetGraphInfo(user.MainDepoID, mCompany.CompanyID, nextDay.ToString("yyyy/MM/dd"), user.DatabaseName);
-                // 納入指示日は翌々日(土日を除く)
-                var nextTwoDay = Utils.GetNextday(nextDay);
-                topModel.NextTwoDayGraph = GetGraphInfo(user.MainDepoID, mCompany.CompanyID, nextTwoDay.ToString("yyyy/MM/dd"), user.DatabaseName);
-
-                // 項目設定
-                topModel.DepoName = string.Concat(user.MainDepoName, " / ", mCompany.CompanyName, "向け");
-
                 return View(topModel);
             }
             catch (Exception ex)
@@ -59,44 +41,5 @@ namespace ai_truck_load_measurement.Controllers
             }
         }
 
-        /// <summary>
-        /// グラフデータを作成
-        /// </summary>
-        /// <param name="mainDepoId">倉庫ID</param>
-        /// <param name="companyId">会社ID</param>
-        /// <param name="searchDate">検索日</param>
-        /// <param name="databaseName">データベース</param>
-        public GraphInfo GetGraphInfo(int mainDepoId, int companyId, string searchDate, string databaseName)
-        {
-            var graphInfo = new GraphInfo();
-            try 
-            {
-                // 出荷指示情報取得
-                var shipmentSql = D_ShipmentScheduleConnectController.CreateSQLToSelectDShipmentSchedulesForWorkProgressInformation(mainDepoId, companyId, searchDate);
-                IEnumerable<D_ShipmentScheduleModel> searchList = D_ShipmentScheduleConnectController.ConnectDShipmentSchedules(shipmentSql, databaseName);
-                var ShipmentScheduleList = searchList.ToPagedList();
-
-                graphInfo.GraphTitle = string.Concat(searchDate, "分　出荷検品数");
-                if (ShipmentScheduleList.Count > 0)
-                {
-                    // 検品済数と未検品数を取得
-                    int scheduleTotal = 0;
-                    int storeOutTotal = 0;
-                    // 計算
-                    foreach (var item in ShipmentScheduleList)
-                    {
-                        scheduleTotal += item.ScheduleNumberOfBoxes;
-                        storeOutTotal += item.StoreOutNumberOfBoxes;
-                    }
-                    graphInfo.ShipmentScheduleTotal = scheduleTotal;
-                    graphInfo.StoreOutTotal = storeOutTotal;
-                }
-                return graphInfo;
-            }
-            catch (Exception)
-            {
-                throw new Exception();
-            }
-        }
     }
 }
--- Models/TopModel.cs
+++ Models/TopModel.cs
@@ -3,54 +3,9 @@
 namespace ai_truck_load_measurement.Models
 {
     /// <summary>
-    /// 倉庫マスターのModel
+    /// トップ画面のModel
     /// </summary>
     public class TopModel : CommonModel
-    {
-        /// <summary>
-        /// 出荷指示リスト
-        /// </summary>
-        public IPagedList<D_ShipmentScheduleModel> ShipmentScheduleList { get; set; }
-
-        /// <summary>
-        /// ハンディエラーメッセージリスト
-        /// </summary>
-        public IPagedList<D_HandyErrorMessageModel> D_HandyErrorMessageList { get; set; }
-
-        /// <summary>
-        /// 倉庫名
-        /// </summary>
-        public string? DepoName { get; set; }
-
-        /// <summary>
-        /// 翌日グラフ情報
-        /// </summary>
-        public GraphInfo NextDayGraph { get; set; }
-
-        /// <summary>
-        /// 翌々日グラフ情報
-        /// </summary>
-        public GraphInfo NextTwoDayGraph { get; set; }
-
-        /// <summary>
-        /// グラフ情報
-        /// </summary>
-        public class GraphInfo
-        {
-            /// <summary>
-            /// 指示箱数
-            /// </summary>
-            public int ShipmentScheduleTotal { get; set; } = 0;
-
-            /// <summary>
-            /// 出庫箱数
-            /// </summary>
-            public int StoreOutTotal { get; set; } = 0;
-
-            /// <summary>
-            /// グラフのタイトル
-            /// </summary>
-            public string? GraphTitle { get; set; } = "";
-        }
+    { 
     }
 }
