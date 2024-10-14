0197a6573a4ae49d59d7828189045607acfc0981
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-10-14 14:20:16 +0900

    [feat]:実績出力画面のMVC要素の叩き台作成

---
+++ Controllers/TruckRecordsOutputController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ai_truck_load_measurement.Controllers
+{
+    public class TruckRecordsOutputController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/TruckRecordsOutputModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ai_truck_load_measurement.Properties;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using X.PagedList;
+
+namespace ai_truck_load_measurement.Models
+{
+    public class TruckRecordsOutputModel : CommonModel
+    {
+        public string? TripName { get; set; }
+
+        public int TripBrunchNumber { get; set; }
+
+        public string? DriverName { get; set; }
+
+        public int StationID { get; set; }
+
+        public int TruckID { get; set; }
+
+        public int IdentifyNumber { get; set; }
+
+        public DateTime ArrivalScheduledTime { get; set; }
+
+        public DateTime DepartureScheduledTime { get; set; }
+
+        public DateTime WorkDay { get; set; }
+
+        public DateTime ArrivedAt { get; set; }
+
+        public DateTime DeparturedAt { get; set; }
+
+        public int ArrivalLoadClass { get; set; }
+
+        public int DepartureLoadClass { get; set; }
+
+        public int RevisionArrivalLoadClass { get; set; }
+
+        public int RevisionDepartureLoadClass { get; set; }
+
+        public string? ArrivalLoadImagePath { get; set; }
+
+        public string? DepartureLoadImagePath { get; set; }
+    }
+}
