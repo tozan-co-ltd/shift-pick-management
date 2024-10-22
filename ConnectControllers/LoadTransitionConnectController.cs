28f9a6a421997a1875a40e54917af1d72ac500a2
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-10-22 16:36:54 +0900

    [feat]:荷量推移ようのMVCを作成

---
+++ ConnectControllers/LoadTransitionConnectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ai_truck_load_measurement.ConnectControllers
+{
+    public class LoadTransitionConnectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LoadTransitionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ai_truck_load_measurement.Controllers
+{
+    public class LoadTransitionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
