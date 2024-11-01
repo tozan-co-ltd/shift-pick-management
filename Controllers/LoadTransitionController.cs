bfd79bc79502b48e52aa4e7261f087b2635b629d
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-11-01 13:43:39 +0900

    [feat]:各実績画面の共通処理用コントローラーを作成、一部処理を移行

--- Controllers/LoadTransitionController.cs
+++ Controllers/LoadTransitionController.cs
@@ -124,8 +124,8 @@ namespace ai_truck_load_measurement.Controllers
                     var loadStatus = new RequestLoadStatus
                     {
                         WorkDay = loadClass.WorkDay,
-                        ArrivalLoadStatus = ConversionLoadClassToLoadStatusForChart(loadClass.ArrivalLoadClass),
-                        DepartureLoadStatus = ConversionLoadClassToLoadStatusForChart(loadClass.DepartureLoadClass)
+                        ArrivalLoadStatus = LoadRecordController.ConversionLoadClassToLoadStatusForChart(loadClass.ArrivalLoadClass),
+                        DepartureLoadStatus = LoadRecordController.ConversionLoadClassToLoadStatusForChart(loadClass.DepartureLoadClass)
                     };
                     loadStatuses.Add(loadStatus);
                 }
@@ -137,26 +137,6 @@ namespace ai_truck_load_measurement.Controllers
                 ViewData["ErrorMessage"] = errorMessage + ex.Message;
                 return loadStatuses;
             }
-        }
-
-
-        /// <summary>
-        /// 荷量クラスからチャート用のパーセント表示に変換
-        /// </summary>
-        /// <param name="loadClass">荷量クラス</param>
-        /// <returns></returns>
-        private string ConversionLoadClassToLoadStatusForChart(int loadClass)
-        {
-            var loadStatus = "";
-            if (loadClass >= 3)
-            { 
-                loadStatus = ((loadClass - 3) * 10 + 5).ToString() ;
-            } 
-            else if(loadClass == 2)
-            {
-                loadStatus = "0";
-            }
-            return loadStatus;
         }
 
         /// <summary>
@@ -284,54 +264,20 @@ namespace ai_truck_load_measurement.Controllers
                 var departureLoadClass = model.DepartureLoadClass;
 
                 // 到着荷量クラスと出発荷量クラスをそれぞれ変換
-                model.ArrivalLoadStatus = ConversionLoadClassToLoadStatus(arrivalLoadClass);
-                model.DepartureLoadStatus = ConversionLoadClassToLoadStatus(departureLoadClass);
+                model.ArrivalLoadStatus = LoadRecordController.ConversionLoadClassToLoadStatus(arrivalLoadClass);
+                model.DepartureLoadStatus = LoadRecordController.ConversionLoadClassToLoadStatus(departureLoadClass);
 
                 // テーブルの空欄を"-"に変換
                 if (string.IsNullOrEmpty(model.TripName)) model.TripName = "-";
                 if (string.IsNullOrEmpty(model.TripBranchSeq)) model.TripBranchSeq = "-";
                 if (string.IsNullOrEmpty(model.DriverName)) model.DriverName = "-";
 
-                model.IdentifyNumber = ConvertNumberToFourDigitOrHyphen(model.IdentifyNumber);
+                model.IdentifyNumber = LoadRecordController.ConvertNumberToFourDigitOrHyphen(model.IdentifyNumber);
             }
             return models;
         }
-        /// <summary>
-        /// 数値を4桁表示またはハイフンに変更する
-        /// </summary>
-        /// <param name="number">変更したい数値</param>
-        /// <returns></returns>
-        private string ConvertNumberToFourDigitOrHyphen(string? number)
-        {
-            if (string.IsNullOrEmpty(number))
-            {
-                return "-";
-            }
-            number = number.PadLeft(4, '0');
-            if (number == "0000") number = "-";
-            return number;
-        }
 
-        /// <summary>
-        /// 荷量クラスからパーセント表示に変換
-        /// </summary>
-        /// <param name="loadClass">荷量クラス</param>
-        /// <returns></returns>
-        private string ConversionLoadClassToLoadStatus(int loadClass)
-        {
-            var loadStatus = "-";
-            if (loadClass >= 3)
-            {
-                int lowerLimit = (loadClass - 3) * 10 + 1;
-                int upperLimit = (loadClass - 2) * 10;
-                loadStatus = ($"{lowerLimit}-{upperLimit}");
-            }
-            else if (loadClass == 2)
-            {
-                loadStatus = "0";
-            }
-            return loadStatus;
-        }
+        
 
         /// <summary>
         /// 荷量画像モーダルに表示する値の取得
@@ -350,73 +296,10 @@ namespace ai_truck_load_measurement.Controllers
             }
 
             // ステーションの画像取得
-            model.ArrivalLoadImgPath = CheckAndConvertImagePath(model.ArrivalLoadImgPath);
-            model.DepartureLoadImgPath = CheckAndConvertImagePath(model.DepartureLoadImgPath);
+            model.ArrivalLoadImgPath = LoadRecordController.CheckAndConvertImagePath(model.ArrivalLoadImgPath);
+            model.DepartureLoadImgPath = LoadRecordController.CheckAndConvertImagePath(model.DepartureLoadImgPath);
 
             return model;
-        }
-
-        /// <summary>
-        /// 画像のパスが正しいかどうかのチェックとパスの変換
-        /// </summary>
-        /// <param name="imagePath">画像パス</param>
-        /// <returns></returns>
-        private string CheckAndConvertImagePath(string imagePath)
-        {
-            // 画像パスに画像がないかパスが不正な場合はダミー画像を表示する
-            if (!IsValidImage(imagePath))
-            {
-                var rootPath = Directory.GetCurrentDirectory();
-                imagePath = Path.Combine(rootPath, @"wwwroot\images\NoImage.png");
-            }
-            var imagePathToBase64 = ImageToBase64(imagePath);
-            return imagePathToBase64;
-        }
-
-        /// <summary>
-        /// 画像のパスをBase64文字列に変換する
-        /// </summary>
-        /// <param name="imagePath">変換したい画像のパス</param>
-        /// <returns></returns>
-        private static string ImageToBase64(string imagePath)
-        {
-            using (Image image = Image.FromFile(imagePath))
-            {
-                using (MemoryStream memoryStream = new MemoryStream())
-                {
-                    image.Save(memoryStream, ImageFormat.Jpeg); // 画像フォーマットを指定（ここではJPEG）
-                    byte[] imageBytes = memoryStream.ToArray();
-                    return Convert.ToBase64String(imageBytes);
-                }
-            }
-        }
-
-        // <summary>
-        /// 画像のパスが正しいかどうか確認する
-        /// </summary>
-        /// <param name="imagePath">確認したい画像パス</param>
-        /// <returns></returns>        
-        public bool IsValidImage(string imagePath)
-        {
-            // 画像パスがここに含まれたフォーマットの場合trueを返す
-            var imageFormats = new List<ImageFormat>()
-                  {
-                    ImageFormat.Jpeg,
-                    ImageFormat.Png,
-                  };
-            try
-            {
-
-                using (FileStream fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
-                using (Image targetImage = Image.FromStream(fileStream))
-                {
-                    return imageFormats.Contains(targetImage.RawFormat);
-                }
-            }
-            catch (Exception)
-            {
-                return false;
-            }
         }
 
         /// <summary>
@@ -455,7 +338,7 @@ namespace ai_truck_load_measurement.Controllers
                 DataTable tTripRecordDT = LoadTransitionConnectController.ConnectTTripRecordToDataTable(tTripRecordSql, "AI-truck-load-measurement_test");
 
                 // 荷量のクラスを数値化
-                tTripRecordDT = GetConvertedLoadClassDataTable(tTripRecordDT);
+                tTripRecordDT = LoadRecordController.GetConvertedLoadClassDataTable(tTripRecordDT);
 
                 // ファイル名
                 var tmpFilename = $"荷量実績_{startDate}-{endDate}.xlsx";
@@ -513,70 +396,6 @@ namespace ai_truck_load_measurement.Controllers
         }
 
         /// <summary>
-        /// データテーブルの荷量クラスを数値に変換
-        /// </summary>
-        /// <param name="dt">変換元データテーブル</param>
-        /// <returns></returns>
-        private DataTable GetConvertedLoadClassDataTable(DataTable dt)
-        {
-            // テーブルに値を変換した後の文字列を格納する列を追加
-            dt.Columns.Add("converted_branch_seq", typeof(string)).SetOrdinal(1);
-            dt.Columns.Add("converted_truck_number", typeof(string)).SetOrdinal(5);
-            dt.Columns.Add("converted_identify_number", typeof(string)).SetOrdinal(6);
-            dt.Columns.Add("converted_arrival_scheduled_time", typeof(string)).SetOrdinal(7);
-            dt.Columns.Add("converted_departure_scheduled_time", typeof(string)).SetOrdinal(8);
-            dt.Columns.Add("arrival_load_status", typeof(string)).SetOrdinal(16);
-            dt.Columns.Add("departure_load_status", typeof(string)).SetOrdinal(17);
-
-            // 各列の値を適切な値に変換
-            foreach (DataRow row in dt.Rows)
-            {
-                // 荷量クラスを%表示に変換
-                var arrivalLoadClass = (int)row["arrival_load_class"];
-                var departureLoadClass = (int)row["departure_load_class"];
-                row["arrival_load_status"] = ConversionLoadClassToLoadStatus(arrivalLoadClass);
-                row["departure_load_status"] = ConversionLoadClassToLoadStatus(departureLoadClass);
-
-                // 各列の値が空白の場合、"-"に変換する
-                if (string.IsNullOrEmpty(row["trip_name"].ToString())) row["trip_name"] = "-";
-                if (string.IsNullOrEmpty(row["driver_name"].ToString())) row["driver_name"] = "-";
-                row["converted_identify_number"] = ConvertNumberToFourDigitOrHyphen(row["identify_number"].ToString());
-                ConvertString(row, "trip_branch_seq", "converted_branch_seq");
-                ConvertString(row, "truck_number", "converted_truck_number");
-                ConvertString(row, "arrival_scheduled_time", "converted_arrival_scheduled_time");
-                ConvertString(row, "departure_scheduled_time", "converted_departure_scheduled_time");
-            }
-
-            // 変換前の列を削除
-            dt.Columns.Remove("trip_branch_seq");
-            dt.Columns.Remove("truck_number");
-            dt.Columns.Remove("identify_number");
-            dt.Columns.Remove("arrival_scheduled_time");
-            dt.Columns.Remove("departure_scheduled_time");
-            dt.Columns.Remove("arrival_load_class");
-            dt.Columns.Remove("departure_load_class");
-            return dt;
-        }
-
-        /// <summary>
-        /// 列の値を文字列に変換して違う列に格納する
-        /// </summary>
-        /// <param name="row">行データ</param>
-        /// <param name="beforeColumnName">変換したい列名</param>
-        /// <param name="afterColumnName">変換後の列名</param>
-        private void ConvertString(DataRow row, string beforeColumnName, string afterColumnName)
-        {
-            if (string.IsNullOrEmpty(row[beforeColumnName].ToString()))
-            {
-                row[afterColumnName] = "-";
-            }
-            else
-            {
-                row[afterColumnName] = row[beforeColumnName].ToString();
-            }
-        }
-
-        /// <summary>
         /// 画像一括ダウンロード
         /// </summary>
         /// <param name="download"></param>
@@ -606,13 +425,13 @@ namespace ai_truck_load_measurement.Controllers
                         foreach (var tripRecord in tripRecordList)
                         {
                             // ステーションの画像取得
-                            var arrivalLoadImgPath = CheckAndConvertImagePath(tripRecord.ArrivalLoadImgPath);
-                            var departureLoadImgPath = CheckAndConvertImagePath(tripRecord.DepartureLoadImgPath);
+                            var arrivalLoadImgPath = LoadRecordController.CheckAndConvertImagePath(tripRecord.ArrivalLoadImgPath);
+                            var departureLoadImgPath = LoadRecordController.CheckAndConvertImagePath(tripRecord.DepartureLoadImgPath);
                             byte[] arrivalLoadImgBytes = Convert.FromBase64String(arrivalLoadImgPath);
                             byte[] departureLoadImgBytes = Convert.FromBase64String(departureLoadImgPath);
                             // 荷量取得
-                            var arrivalLoadStatus = ConversionLoadClassToLoadStatus(tripRecord.ArrivalLoadClass);
-                            var departureLoadStatus = ConversionLoadClassToLoadStatus(tripRecord.DepartureLoadClass);
+                            var arrivalLoadStatus = LoadRecordController.ConversionLoadClassToLoadStatus(tripRecord.ArrivalLoadClass);
+                            var departureLoadStatus = LoadRecordController.ConversionLoadClassToLoadStatus(tripRecord.DepartureLoadClass);
                             date = tripRecord.WorkDay.ToString("yyyyMMdd");
                             // 便名称と便枝番が空欄の時の処理
                             var tripName = tripRecord.TripName;
