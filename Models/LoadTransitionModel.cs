7874e027508b41729f4b59a075ce5559d194db2a
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-11-01 17:09:43 +0900

    [docs]:モデルのサマリ追加

--- Models/LoadTransitionModel.cs
+++ Models/LoadTransitionModel.cs
@@ -5,14 +5,22 @@ namespace ai_truck_load_measurement.Models
 {
     public class LoadTransitionModel : LoadRecordModel
     {
+        // 便名称のリスト
         public List<SelectListItem>? TripNameList { get; set; }
+        // 選択された便名称
         public string? SelectedTripName {  get; set; }
     }
 
+    /// <summary>
+    /// 検索された便実績データ
+    /// </summary>
     public class RequestLoadStatus
     {
+        // 到着荷量の%表示
         public string? ArrivalLoadStatus { get; set; }
+        // 出発荷量の%表示
         public string? DepartureLoadStatus { get; set; }
+        // 稼働日
         public DateTime WorkDay { get; set; }
     }
 
