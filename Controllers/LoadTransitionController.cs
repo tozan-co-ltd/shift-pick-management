0145ec14d1e5d2f39e02c17750f76cef864cc86b
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-11-06 10:53:51 +0900

    [fix]:一部文言とファイル名の修正、実環境でエクセル出力がうまく動作しない不具合を修正

--- Controllers/LoadTransitionController.cs
+++ Controllers/LoadTransitionController.cs
@@ -344,7 +344,7 @@ namespace ai_truck_load_measurement.Controllers
                 DataTable searchConditionDT = new DataTable();
                 searchConditionDT.Columns.Add("項目名");
                 searchConditionDT.Columns.Add("検索条件");
-                searchConditionDT.Rows.Add("期間", $"{startDate}～{endDate}");
+                searchConditionDT.Rows.Add("稼働日", $"{startDate}～{endDate}");
                 var selectedTripNames = "";
                 for(int i=0; i<arrayTrips.Count; i++)
                 {
