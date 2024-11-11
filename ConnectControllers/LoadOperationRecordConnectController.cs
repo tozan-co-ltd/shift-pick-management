9c074742aeda755fdcbfd2b1ce562aaab2ab86fe
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-11-11 14:30:01 +0900

    [feat]:荷量と運行実績用のMVC作成

---
+++ ConnectControllers/LoadOperationRecordConnectController.cs
@@ -0,0 +1,32 @@
+﻿namespace ai_truck_load_measurement.ConnectControllers
+{
+    public class LoadOperationRecordConnectController 
+    {
+        public static string CreateSQLToSelectTripNameFromWorkDays(DateTime workDay)
+        {
+            var sql = $@"
+                SELECT DISTINCT
+	                trip_name AS Value,
+	                trip_name AS Text
+                FROM t_trip_records
+                WHERE work_day = '{workDay.ToString("yyyy/MM/dd")}'
+                AND trip_name IS NOT NULL
+";
+            return sql;
+        }
+
+        private static string SelectedDaysSQL(List<DateTime> days)
+        {
+            var selectedDays = "";
+            for(int i=0; i<days.Count; i++)
+            {
+                if(i != 0)
+                {
+                    selectedDays += " OR ";
+                }
+                selectedDays += $"work_day = '{days[i].ToString("yyyy/MM/dd")}'";
+            }
+            return selectedDays;
+        }
+    }
+}
---
+++ Controllers/LoadOperationRecordController.cs
@@ -0,0 +1,69 @@
+﻿using ai_truck_load_measurement.ConnectControllers;
+using ai_truck_load_measurement.Models;
+using ai_truck_load_measurement.Properties;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using X.PagedList;
+
+namespace ai_truck_load_measurement.Controllers
+{
+    public class LoadOperationRecordController : BaseController
+    {
+        public IActionResult Index()
+        {
+            var model = new LoadOperationRecordModel();
+            var today = DateTime.Now;
+            try
+            {
+                // 便実績情報取得SQL作成
+                var sql = LoadOperationRecordConnectController.CreateSQLToSelectTripNameFromWorkDays(today);
+                // DB接続
+                List<SelectListItem> tripNameList = LoadDistributionConnectController.ConnectTTripRecordsForTripName(sql);
+
+                model.TripNameList = tripNameList;
+
+                // 便実績情報取得SQL作成
+                var sql2 = LoadRecordConnectController.CreatSQLToSelectTripRecord();
+                // DB接続
+                IEnumerable<LoadDistributionModel> tripRecordList = LoadDistributionConnectController.ConnectTTripRecords(sql2);
+                // テーブル情報を変換
+                tripRecordList = (IEnumerable<LoadDistributionModel>)LoadRecordController.ConversionForTable(tripRecordList);
+
+                model.TripRecordList = tripRecordList.ToPagedList();
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = "E9999: " + ErrorMessagesResources.E9999;
+                ViewData["ErrorMessage"] = errorMessage + ex.Message;
+                return View(model);
+            }
+        }
+
+            /// <summary>
+            /// 指定した期間内に存在する便名称のリストを取得してセレクトリストアイテム化する
+            /// </summary>
+            /// <param name="startOfPeriod">期間の開始日時</param>
+            /// <param name="endOfPeriod">期間の終了日時</param>
+            /// <returns></returns>
+            public List<SelectListItem> GetTripNameFromWorkDays(DateTime workDay)
+        {
+            List<SelectListItem> tripRecordList = new();
+            try
+            {
+                // 便実績情報取得SQL作成
+                var sql = LoadOperationRecordConnectController.CreateSQLToSelectTripNameFromWorkDays(workDay);
+                // DB接続
+                tripRecordList = LoadTransitionConnectController.ConnectTTripRecordsForTripName(sql);
+
+                return tripRecordList;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = "E9999: " + ErrorMessagesResources.E9999;
+                ViewData["ErrorMessage"] = errorMessage + ex.Message;
+                return tripRecordList;
+            }
+        }
+    }
+}
