cb5e1f3a63c1265ca7a5d3c99a23de6f22d3d88b
tozan-yoshida <toru_yoshida@tozan.co.jp>
2024-11-27 15:23:49 +0900

    [fix]:ステーション状況取得先テーブルをt_tmp_station_statusからt_load_detect_recordsに変更

--- ConnectControllers/TopConnectController.cs
+++ ConnectControllers/TopConnectController.cs
@@ -48,9 +48,16 @@ namespace ai_truck_load_measurement.ConnectControllers
         {
             var sql = $@"
                 SELECT 
-	                station_id,
-	                load_class
-                FROM t_tmp_station_status
+                     detect_records.station_id,
+                     load_class
+                 FROM t_load_detect_records AS detect_records
+                 JOIN (
+                    SELECT station_id, MAX(created_at) AS latest_create
+                    FROM t_load_detect_records
+                    GROUP BY station_id
+                ) latest_detect_records
+                ON detect_records.station_id = latest_detect_records.station_id 
+                AND detect_records.created_at = latest_detect_records.latest_create
             ";
             return sql;
         }
@@ -69,9 +76,9 @@ namespace ai_truck_load_measurement.ConnectControllers
 	                SELECT station_id, MAX(created_at) AS latest_create
 	                FROM t_truck_sensor_records
 	                GROUP BY station_id
-                ) latest_detect_recprds
-                ON truck_sensor_records.station_id = latest_detect_recprds.station_id 
-                AND truck_sensor_records.created_at = latest_detect_recprds.latest_create
+                ) latest_detect_records
+                ON truck_sensor_records.station_id = latest_detect_records.station_id 
+                AND truck_sensor_records.created_at = latest_detect_records.latest_create
             ";
             return sql;
         }
