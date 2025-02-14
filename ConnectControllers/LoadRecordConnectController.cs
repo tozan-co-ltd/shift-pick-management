97ad51408944cf8a280a3c0ebbcc49fd14f63e30
tozan-yoshida <toru_yoshida@tozan.co.jp>
2025-02-14 09:22:31 +0900

    [recact]:参照されていない関数を削除

--- ConnectControllers/LoadRecordConnectController.cs
+++ ConnectControllers/LoadRecordConnectController.cs
@@ -292,37 +292,6 @@ namespace ai_truck_load_measurement.ConnectControllers
             }
         }
 
-        /// <summary>
-        /// デポ情報取得
-        /// </summary>
-        /// <param name="sql"></param>
-        /// <returns></returns>
-        public static List<M_DepoModel> ConnectMDepos(string sql)
-        {
-            // 戻り値
-            List<M_DepoModel> strList = new();
-
-            // DB接続
-            try
-            {
-                // SQLServer接続文字列取得
-                var connectionString = ConnectToSQLServer.GetSQLServerConnectionString();
-                // SQLServer接続
-                using (var connection = new SqlConnection())
-                {
-                    connection.ConnectionString = connectionString;
-                    connection.Open();
-                    Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-                    strList = connection.Query<M_DepoModel>(sql).ToList();
-                }
-                return strList;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
 
         /// <summary>
         /// 便実績情報取得SQL
@@ -503,22 +472,7 @@ namespace ai_truck_load_measurement.ConnectControllers
             return sql;
         }
 
-        /// <summary>
-        /// デポ情報取得SQL
-        /// </summary>
-        /// <returns></returns>
-        public static string CreateSQLToSelectMDepos()
-        {
-            var sql = $@"
-                SELECT
-                    depo_id,
-                    name
-                FROM
-                    m_depos
-            ";
-            return sql;
-        }
-
+       
         /// <summary>
         /// 荷量の相違ありテーブルの到着出発クラスに保存する値
         /// </summary>
--- Models/M_DepoModel.cs
+++ Models/M_DepoModel.cs
@@ -1,4 +1,7 @@
-﻿namespace ai_truck_load_measurement.Models
+﻿using ai_truck_load_measurement.Models;
+using ai_truck_load_measurement.ConnectControllers;
+
+namespace ai_truck_load_measurement.Models
 {
     /// <summary>
     /// デポモデル
@@ -17,5 +20,19 @@
         public DateTime UpdatedAt { get; set; }
         // 更新者
         public string UpdatedBy { get; set; }
+
+        
+        public static M_DepoModel GetMainDepo(LoginUserModel user)
+        {
+            M_DepoModel model = new M_DepoModel();
+            var sql = M_DepoConnectController.CreateSQLToSelectDepoFromADName(user.ADName);
+            var depoList = M_DepoConnectController.ConnectMDepos(sql);
+            if (depoList.Count > 0)
+            {
+                model = depoList[0];
+            }
+            return model;
+            
+        }
     }
 }
