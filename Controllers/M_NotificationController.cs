b64323b1bd2c821bad8191060ad99464d3367e27
tozan-yoshida <toru_yoshida@tozan.co.jp>
2025-04-21 10:43:20 +0900

    [feat]:通知マスター用モデルと通知マスター画面の叩き台作成

---
+++ Controllers/M_NotificationController.cs
@@ -0,0 +1,14 @@
+﻿using ai_truck_load_measurement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ai_truck_load_measurement.Controllers
+{
+    public class M_NotificationController : BaseController
+    {
+        public IActionResult Index()
+        {
+            var model = new M_NotificationViewModel();
+            return View(model);
+        }
+    }
+}
