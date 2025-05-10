5dd26ab5bad33eaa2288254842c1201f92b5a8e4
tozan-yoshida <toru_yoshida@tozan.co.jp>
2025-05-10 15:00:20 +0900

    [feat]:アラート履歴画面表示用のビューとコントローラーを作成

---
+++ Controllers/AlertRecordController.cs
@@ -0,0 +1,14 @@
+﻿using ai_truck_load_measurement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ai_truck_load_measurement.Controllers
+{
+    public class AlertRecordController : BaseController
+    {
+        public IActionResult Index()
+        {
+            var model = new AlertRecordModel();
+            return View(model);
+        }
+    }
+}
--- Models/WebMenuModel.cs
+++ Models/WebMenuModel.cs
@@ -17,6 +17,7 @@ namespace ai_truck_load_measurement.Models
             new M_WebMenu{CategoryID = 1, MenuID = 2, MenuName = "荷量推移", Controller = "LoadTransition", Action = "Index"},
             new M_WebMenu{CategoryID = 1, MenuID = 3, MenuName = "荷量と運行実績", Controller = "LoadOperationRecord", Action = "Index"},
             new M_WebMenu{CategoryID = 1, MenuID = 4, MenuName = "実績出力", Controller = "LoadOutput", Action = "Index"},
+            new M_WebMenu{CategoryID = 1, MenuID = 5, MenuName = "アラート履歴", Controller = "AlertRecord", Action = "Index"},
             new M_WebMenu{CategoryID = 2, MenuID = 1, MenuName = "車両マスター", Controller = "M_Truck", Action = "Index"},
             new M_WebMenu{CategoryID = 2, MenuID = 2, MenuName = "便マスター", Controller = "M_Trip", Action = "Index"},
             new M_WebMenu{CategoryID = 2, MenuID = 3, MenuName = "便枝番マスター", Controller = "M_TripBranchNumber", Action = "Index"},
