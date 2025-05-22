0572052d6f7630fca91ad3a4dbdb80a3e314ba3a
tozan-yoshida <toru_yoshida@tozan.co.jp>
2025-05-22 17:09:25 +0900

    [feat]:お知らせ画面のmvcを作成

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,14 @@
+﻿using ai_truck_load_measurement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ai_truck_load_measurement.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            NewsListViewModel model = new();
+            return View(model);
+        }
+    }
+}
---
+++ Models/NewsModel.cs
@@ -0,0 +1,26 @@
+﻿namespace ai_truck_load_measurement.Models
+{
+    public class NewsModel : CommonModel
+    {
+        public int NewsID {  get; set; }
+
+        public string NewsSubject {  get; set; }
+        public string NewsContent {  get; set; }
+        public int NewsGenreClass {  get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy {  get; set; }
+
+    }
+
+    public class NewsListViewModel : CommonModel
+    {
+        public List<NewsModel> NewsList { get; set; }
+        public int NewsID { get; set; }
+        public string NewsSubject { get; set; }
+        public string NewsContent { get; set; }
+        public int NewsGenreClass { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+
+    }
+}
