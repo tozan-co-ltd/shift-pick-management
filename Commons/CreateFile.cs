f7bc1b3089af3556d2699dd884aa6f162e27867b
tozan-yoshida <toru_yoshida@tozan.co.jp>
2025-07-08 15:59:15 +0900

    [feat]:実績出力画面のシートの順序を入れ替え

--- Commons/CreateFile.cs
+++ Commons/CreateFile.cs
@@ -720,8 +720,23 @@ namespace ai_truck_load_measurement.Commons
                     headerList.Add("更新者");
                     break;
                 default:
-                    headerList.Add("項目名");
-                    headerList.Add("検索条件");
+                    headerList.Add("便名称");
+                    headerList.Add("便枝番");
+                    headerList.Add("タグ");
+                    headerList.Add("乗務員");
+                    headerList.Add("ステーション名");
+                    headerList.Add("車両番号");
+                    headerList.Add("識別番号");
+                    headerList.Add("デポ名");
+                    headerList.Add("到着予定時間");
+                    headerList.Add("出発予定時間");
+                    headerList.Add("稼働日");
+                    headerList.Add("到着実績");
+                    headerList.Add("出発実績");
+                    headerList.Add("到着荷量(%)");
+                    headerList.Add("出発荷量(%)");
+                    headerList.Add("到着荷量画像パス");
+                    headerList.Add("出発荷量画像パス");
                     break;
             }
             return headerList;
@@ -792,23 +807,8 @@ namespace ai_truck_load_measurement.Commons
                     headerList.Add("作成者");
                     break;
                 default:
-                    headerList.Add("便名称");
-                    headerList.Add("便枝番");
-                    headerList.Add("タグ");
-                    headerList.Add("乗務員");
-                    headerList.Add("ステーション名");
-                    headerList.Add("車両番号");
-                    headerList.Add("識別番号");
-                    headerList.Add("デポ名");
-                    headerList.Add("到着予定時間");
-                    headerList.Add("出発予定時間");
-                    headerList.Add("稼働日");
-                    headerList.Add("到着実績");
-                    headerList.Add("出発実績");
-                    headerList.Add("到着荷量(%)");
-                    headerList.Add("出発荷量(%)");
-                    headerList.Add("到着荷量画像パス");
-                    headerList.Add("出発荷量画像パス");
+                    headerList.Add("項目名");
+                    headerList.Add("検索条件");
                     break;
             }
             return headerList;
