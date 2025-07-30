b12ba12d14d159b7edd5f06bfee9c8aefffc7bc5
tozan-yoshida <toru_yoshida@tozan.co.jp>
2025-07-30 11:25:28 +0900

    [feat]:便予実進捗のMVC作成

---
+++ Controllers/LoadProgressController.cs
@@ -0,0 +1,21 @@
+﻿using ai_truck_load_measurement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ai_truck_load_measurement.Controllers
+{
+    public class LoadProgressController : BaseController
+    {
+        public IActionResult Index()
+        {
+            LoadProgressModel model = new();
+            var user = ClaimsLoginUserData();
+            model.SelectedDepo = new M_DepoModel { 
+                DepoID = user.MainDepoID,
+                Name = user.MainDepoName
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Models/LoadProgressModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ai_truck_load_measurement.Models
+{
+    public class LoadProgressModel
+    {
+        // 便実績リスト
+        public List<LoadRecordModel> LoadRecords { get; set; }
+        // 便予定リスト
+        public List<M_TripModel> TripRecords { get; set; }
+        // 稼働日
+        public DateTime WorkDay {  get; set; }
+        // 現在時刻
+        public DateTime WorkTime { get; set; }
+        // 選択デポ
+        public M_DepoModel SelectedDepo { get; set; }
+    }
+}
